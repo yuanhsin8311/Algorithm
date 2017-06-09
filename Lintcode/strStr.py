03f5f772c11e0b5170924124ef389dde2144c01f
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-06-09 16:36:49 -0700

    Create strStr.py

---
+++ Lintcode/strStr.py
@@ -0,0 +1,28 @@
+'''
+@Copyright:LintCode
+@Author:   cindy huang
+@Problem:  http://www.lintcode.com/problem/strstr
+@Language: Python
+@DateTime: 2017.06.09
+'''
+
+class Solution:
+    def strStr(self, source, target):
+        # write your code here
+        
+        if target is None or source is None:
+            return -1
+            
+        if len(target) == 0:
+            return 0
+            
+        for i in xrange(len(source) - len(target)+1):
+            for j in xrange(len(target)):
+                if source[i+j] != target[j]:
+                    break;
+                    
+            if j == len(target) - 1 and source[i+j] == target[j]:
+                return i
+                
+        return -1
+        
