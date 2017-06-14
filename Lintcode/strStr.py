8f7fe293b8fb6ae93e6a8727c4a333391a4877f6
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-06-14 16:00:03 -0700

    Update strStr.py

--- Lintcode/strStr.py
+++ Lintcode/strStr.py
@@ -26,3 +26,15 @@ class Solution:
                 
         return -1
         
+class Solution:
+    def strStr(self, source, target):
+        if source is None or target is None:
+            return -1
+
+        for i in range(len(source) - len(target) + 1):
+            for j in range(len(target)):
+                if source[i + j] != target[j]:
+                    break
+            else:  # no break
+                return i
+        return -1
