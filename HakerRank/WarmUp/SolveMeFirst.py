710b8a08e49cb2c9c1ff9bec8faafb4812de6c27
Yuan-Hsin Huang <yuanhsin8311@gmail.com>
2017-06-28 11:20:38 -0700

    Create SolveMeFirst.py

---
+++ HakerRank/WarmUp/SolveMeFirst.py
@@ -0,0 +1,31 @@
+/*****************************************************************************************************************************
+The purpose of this challenge is to familiarize you with reading input from stdin (the standard input stream) 
+and writing output to stdout (the standard output stream) using our environment.
+
+Review the code provided in the editor below, then complete the solveMeFirst function so that it returns the 
+sum of two integers read from stdin. Take some time to understand this code so you're prepared to write it 
+yourself in future challenges.
+
+Select a language below, and start coding!
+
+Input Format
+
+Code that reads input from stdin is provided for you in the editor. 
+There are  lines of input, and each line contains a single integer.
+
+Output Format
+
+Code that prints the sum calculated and returned by solveMeFirst is provided for you in the editor.
+****************************************************************************************************************************/
+
+def solveMeFirst(a,b):
+   # Hint: Type return a+b below 
+    return a+b
+
+
+num1 = input()
+num2 = input()
+res = solveMeFirst(num1,num2)
+print res
+
+
